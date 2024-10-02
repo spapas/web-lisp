c061fbe7a25175e38b70f866aedc1f94d3a5324e
Serafeim Papastefanos <spapas@gmail.com>
2024-10-02 18:47:55 +0300

    hm

--- main.lisp
+++ main.lisp
@@ -20,10 +20,10 @@
   (let* ((uri (ht:request-uri request))
          (method (ht:request-method request)))
     (ht:log-message* :INFO
-      (concatenate 'string
-        "REQUEST:~%Method: " (format nil "~A" method) "~%"
-        "URI: " (format nil "~A" uri) "~%"
-        "SESSION: " (format nil "~A" (debug-session ht:*session*)) "~%"))))
+                     (concatenate 'string
+                       "REQUEST:~%Method: " (format nil "~A" method) "~%"
+                       "URI: " (format nil "~A" uri) "~%"
+                       "SESSION: " (format nil "~A" (debug-session ht:*session*)) "~%"))))
 
 (defclass slash-redirect-acceptor (easy-routes:easy-routes-acceptor)
     ()
@@ -42,20 +42,20 @@
 
 (defmethod session-created ((acceptor slash-redirect-acceptor) (session t))
   (ht:log-message* :INFO "Session created: ~A // ~A // ~Α"
-    session
-    session ht:*session*
-    (ht:session-id session))
+                   session
+                   session ht:*session*
+                   (ht:session-id session))
   (call-next-method))
 
 (defvar *base-acceptor* (make-instance
-                          'slash-redirect-acceptor
+                            'slash-redirect-acceptor
                           :port (get-conf :bind-port)
                           :address (get-conf :bind-address)))
 
 ;; serve static files
 (push (ht:create-folder-dispatcher-and-handler
-        "/static/"
-        #p"c:/progr/lisp/projects/web-lisp/static/")
+       "/static/"
+       #p"c:/progr/lisp/projects/web-lisp/static/")
       ht:*dispatch-table*)
 
 (defun start ()
@@ -69,23 +69,24 @@
   (terpri)
   (start)
 
-  (flet ((cleanup ()
-                  (format t "Stopping acceptor~%")
-                  (ht:stop *base-acceptor*)))
+  (flet
+      ((cleanup ()
+                (format t "Stopping acceptor~%")
+                (ht:stop *base-acceptor*)))
 
-    ;; Set up a handler for the SIGINT signal (Ctrl+C)
-    (handler-bind ((sb-sys:interactive-interrupt
-                    (lambda (condition)
-                      (declare (ignore condition))
-                      (cleanup) ; Call cleanup when Ctrl+C is pressed
-                      (sb-ext:exit))))
+    (when (find :sbcl *features*)
+          (handler-bind ((sb-sys:interactive-interrupt
+                          (lambda (condition)
+                            (declare (ignore condition))
+                            (cleanup) ; Call cleanup when Ctrl+C is pressed
+                            (sb-ext:exit))))))
 
-      ; Join the listener-thread to avoid killing it
-      (sb-thread:join-thread
-        (find-if
-            (lambda (th)
-              (alexandria:starts-with-subseq "hunchentoot-listener" (sb-thread:thread-name th)))
-            (sb-thread:list-all-threads))))))
+    ; Join the listener-thread to avoid killing it
+    (sb-thread:join-thread
+      (find-if
+          (lambda (th)
+            (alexandria:starts-with-subseq "hunchentoot-listener" (sb-thread:thread-name th)))
+          (sb-thread:list-all-threads)))))
 
 
 ;; Start at repl
