9439e5866c0a3b13d1b9c897c1ea64b4db325009
Serafeim Papastefanos <spapas@gmail.com>
2024-10-04 16:47:18 +0300

    Ok

--- auth.lisp
+++ auth.lisp
@@ -52,55 +52,8 @@
 ;37|pbkdf2_sha256$720000$scRIdcYhttb09OmIodwIvW$Auj9fYAbjw9mqvsONL9+8VALmUb7ZQztYK3RyKbpA6c=
 
 
-(ql:quickload 'cl-forms)
-(ql:quickload 'cl-who)
-(ql:quickload 'cl-forms.who)
-(ql:quickload 'cl-forms.who.bootstrap)
 
-(cl-forms:defform fields-form (:action "/fields-post" :method :get)
-                  ((name :string :value "")
-                   (ready :boolean :value t)
-                   (sex :choice :choices (list "Male" "Female") :value "Male")
-                   (submit :submit :label "Create")))
 
-(let ((form (cl-forms::find-form 'fields-form)))
-  (forms:with-form-renderer :who
-                            (forms:render-form form)))
-
-(let ((form (forms:find-form 'fields-form)))
-  (forms:handle-request form)
-  (forms:with-form-field-values (name ready sex) form
-                                (who:with-html-output (forms.who::*html*)
-                                                      (:ul
-                                                       (:li (who:fmt "Name: ~A" name))
-                                                       (:li (who:fmt "Ready: ~A" ready))
-                                                       (:li (who:fmt "Sex: ~A" sex))))))
-
-(with-output-to-string (forms.who:*html*)
-  (let ((form (forms:find-form 'fields-form)))
-    (forms:with-form-renderer :who
-                              (forms:render-form form))))
-(defvar *html*)
-(let ((form (forms:find-form 'fields-form)))
-  (with-output-to-string (*html*)
-    (spinneret:with-html
-     (let ((cl-forms.who:*html* *html*)
-           (spinneret:*html* *html*))
-       (:h1 "This is my form")
-       (forms:with-form-renderer :who
-                                 (forms:render-form form))
-       (:ul
-        (loop for i from 1 to 10
-              do (:li (princ-to-string i))))))))
-
-(defvar *db* (clsql:connect '("db.sqlite3") :database-type :sqlite3))
-(defvar *query* "SELECT * FROM users WHERE age > ?")
-
-;; Execute the query with a parameter
-(defvar *results* (clsql:query "SELECT * FROM users_user WHERE id > 18" :database *db*))
-;; Process and print the results
-(dolist (row *results*)
-  (format t "User: ~A~%" row))
 
 (defun create-superuser ()
   "Creates a superuser "
