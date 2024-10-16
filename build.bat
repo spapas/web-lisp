152ceb9ddce2704744a28cccfc7833cc188f5024
Serafeim Papastefanos <spapas@gmail.com>
2024-10-16 13:13:53 +0300

    OK

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
 REM sbcl --non-interactive --eval "(ql:quickload :web-lisp)" --eval "(asdf:make :web-lisp)" 
-sbcl --non-interactive --load web-lisp.asd --eval "(asdf:load-system :web-lisp)" --eval "(asdf:make :web-lisp)" 
+sbcl --noinform --non-interactive --load web-lisp.asd ^
+  --eval "(setf *compile-verbose* nil)" ^
+  --eval "(with-open-file (*error-output* \"warnings.log\" :direction :output :if-exists :supersede) (asdf:load-system :web-lisp))" ^
+  --eval "(asdf:make :web-lisp)"
