884f7a3d57fd9e174975ca6061a491e0e8c242e5
Serafeim Papastefanos <spapas@gmail.com>
2024-10-16 13:12:07 +0300

    Silence stuff

--- t/run_tests.bat
+++ t/run_tests.bat
@@ -1,1 +1,5 @@
-sbcl --non-interactive --load web-lisp.asd --eval "(asdf:load-system :web-lisp/tests)" --eval "(web-lisp-tests:run-tests)" 
+REM sbcl --non-interactive  --load web-lisp.asd --eval "(asdf:load-system :web-lisp/tests)" --eval "(web-lisp-tests:run-tests)" 
+sbcl --noinform --non-interactive  --load web-lisp.asd ^
+  --eval "(setf *compile-verbose* nil)" ^
+  --eval "(with-open-file (*error-output* \"warnings.log\" :direction :output :if-exists :supersede) (asdf:load-system :web-lisp/tests))" ^
+  --eval "(web-lisp-tests:run-tests)" 
